bcd57ce46434420c00c01c8040ea28a1c248438b
IvoMart <ivo_ivancho@hotmail.com>
2022-11-02 00:21:53 -0300

    api.php - All endpoint from BookController

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('books', BookController::class);
